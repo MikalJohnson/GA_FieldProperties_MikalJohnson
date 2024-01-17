9d2d596109d5350155f1fbfbca2bc76aa86f8a39
MikalJohnson <Johnsonmikal24@gmail.com>
2024-01-16 21:48:38 -0800

    Add project files.

---
+++ GA_FieldProperties_MikalJohnson/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldProperties_MikalJohnson
+{
+    internal class CustomerReview
+    {
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                    _name = value;
+                else
+                    throw new ArgumentException("Name cannot be null or empty");
+                }
+            }
+
+        public string readOnlyName
+        {
+            get { return _name; }
+        }
+        public string writeOnlyName
+        {
+            set { _name = value; }
+        }
+        public string writtenreview;
+        public int rating;
+    }
+}
---
+++ GA_FieldProperties_MikalJohnson/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GA_FieldProperties_MikalJohnson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Understanding Class Fields and Properties");
+
+            CustomerReview customerReview = new CustomerReview();
+            customerReview.name = "Mikal";
+            Console.WriteLine(customerReview.name);
+            Console.WriteLine(customerReview.readOnlyName);
+        } 
+    }   
+}
